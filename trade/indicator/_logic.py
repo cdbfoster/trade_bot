d99ac97cbc0bda77b0be3dcb4a33adc2cdf0818e
Chris Foster <cdbfoster@gmail.com>
2018-05-30 22:52:03 -0400

    Updated the logic indicators.

--- trade/indicator/_logic.py
+++ trade/indicator/_logic.py
@@ -13,75 +13,74 @@
 # You should have received a copy of the GNU General Public License
 # along with trade_bot.  If not, see <http://www.gnu.org/licenses/>.
 
-from trade.indicator import Indicator, Signal
+from trade.indicator import Indicator, IndicatorInput, Signal
 
 class And(Indicator):
     def __init__(self, input1, input2):
-        self.input = input1
-        self.input1 = input1
-        self.input2 = input2
+        self.__input1 = IndicatorInput(input1)
+        self.__input2 = IndicatorInput(input2)
 
         Indicator.__init__(self)
 
     def _next(self):
-        self.input1._update()
-        self.input2._update()
+        self.inputs.update()
 
-        input1_shift = max(len(self.input1) - len(self.input2), 0)
-        input2_shift = max(len(self.input2) - len(self.input1), 0)
-
-        input_index = len(self)
-        if input_index + input1_shift >= len(self.input1) or input_index + input2_shift >= len(self.input2):
+        if len(self) >= min(len(self.__input1), len(self.__input2)):
             raise StopIteration
 
-        signal = self.input1[input_index + input1_shift]
-        if signal != self.input2[input_index + input2_shift]:
+        self.inputs.sync_to_min_length()
+
+        input1 = self.__input1.consume()
+        input2 = self.__input2.consume()
+
+        signal = input1
+        if signal != input2:
             signal = Signal.HOLD
 
         self._values.append(signal)
 
 class Not(Indicator):
     def __init__(self, input_):
-        self.input = input_
+        self.__input = IndicatorInput(input_)
 
         Indicator.__init__(self)
 
     def _next(self):
-        self.input._update()
+        self.inputs.update()
 
-        input_index = len(self)
-        if input_index >= len(self.input):
+        if len(self) >= len(self.__input):
             raise StopIteration
 
+        input_ = self.__input.consume()
+
         signal = Signal.HOLD
-        if self.input[input_index] is Signal.BUY:
+        if input_ is Signal.BUY:
             signal = Signal.SELL
-        elif self.input[input_index] is Signal.SELL:
+        elif input_ is Signal.SELL:
             signal = Signal.BUY
 
         self._values.append(signal)
 
 class Or(Indicator):
     def __init__(self, input1, input2):
-        self.input = input1
-        self.input1 = input1
-        self.input2 = input2
+        self.__input1 = IndicatorInput(input1)
+        self.__input2 = IndicatorInput(input2)
 
         Indicator.__init__(self)
 
     def _next(self):
-        self.input1._update()
-        self.input2._update()
+        self.inputs.update()
 
-        input1_shift = max(len(self.input1) - len(self.input2), 0)
-        input2_shift = max(len(self.input2) - len(self.input1), 0)
-
-        input_index = len(self)
-        if input_index + input1_shift >= len(self.input1) or input_index + input2_shift >= len(self.input2):
+        if len(self) >= min(len(self.__input1), len(self.__input2)):
             raise StopIteration
 
-        signal = self.input1[input_index + input1_shift]
+        self.inputs.sync_to_min_length()
+
+        input1 = self.__input1.consume()
+        input2 = self.__input2.consume()
+
+        signal = input1
         if signal == Signal.HOLD:
-            signal = self.input2[input_index + input2_shift]
+            signal = input2
 
         self._values.append(signal)
